6d4a71a4eda1f3be76f8b17c46c30638db76af55
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-30 17:26:06 +0530

    Modified : method to find maximum value among floats

--- src/com/bridgelabz/findmaximum/FindMaximum.java
+++ src/com/bridgelabz/findmaximum/FindMaximum.java
@@ -4,24 +4,43 @@ public class FindMaximum
 {
 	public static void main(String[] args)
 	{
-		Integer number1 = 30;
-		Integer number2 = 10;
-		Integer number3 = 80;
-		findMaximumNumber(number1, number2, number3);
+		Integer intNumber1 = 30;
+		Integer intNumber2 = 100;
+		Integer intNumber3 = 80;
+		
+		Double doubleNumber1 = 800.3; 
+		Double doubleNumber2 = 800.0; 
+		Double doubleNumber3 = 800.90; 
+		
+		findMaximumIntegerNumber(intNumber1, intNumber2, intNumber3);
+		findMaximumDoubleNumber(doubleNumber1, doubleNumber2, doubleNumber3);
 	}
 
-	private static void findMaximumNumber(Integer number1, Integer number2, Integer number3) 
-	{
-		Integer maximumNumber = number1;
-		if (Integer.compare(maximumNumber, number2) == -1)
+	private static void findMaximumDoubleNumber(Double doubleNumber1, Double doubleNumber2,
+			Double doubleNumber3) {
+		Double maximumNumber = doubleNumber1;
+		if (Double.compare(maximumNumber, doubleNumber2) == -1)
 		{
-			maximumNumber = number2;
+			maximumNumber = doubleNumber2;
 		}
-		if (Integer.compare(maximumNumber, number3) == -1)
+		if (Double.compare(maximumNumber, doubleNumber3) == -1)
 		{
-			maximumNumber = number3;
+			maximumNumber = doubleNumber3;
 		}
+		System.out.println("The maximum floating Number is "+ maximumNumber);
+	}
 
-		System.out.println("The maximum Number is "+ maximumNumber);
+	private static void findMaximumIntegerNumber(Integer intNumber1, Integer intNumber2, Integer intNumber3) 
+	{
+		Integer maximumNumber = intNumber1;
+		if (Integer.compare(maximumNumber, intNumber2) == -1)
+		{
+			maximumNumber = intNumber2;
+		}
+		if (Integer.compare(maximumNumber, intNumber3) == -1)
+		{
+			maximumNumber = intNumber3;
+		}
+		System.out.println("The maximum Integer Number is "+ maximumNumber);
 	}
 }
