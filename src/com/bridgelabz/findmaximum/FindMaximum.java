3471277d015124f09c0390f4806a29c1b6bee1b6
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-30 19:12:01 +0530

    Added : generic class to take 3 variables

--- src/com/bridgelabz/findmaximum/FindMaximum.java
+++ src/com/bridgelabz/findmaximum/FindMaximum.java
@@ -1,24 +1,22 @@
 package com.bridgelabz.findmaximum;
 
-public class FindMaximum 
+public class FindMaximum <T extends Comparable<T>>
 {
-	public static void main(String[] args)
+	private T value1;
+	private T value2;
+	private T value3;
+
+	public FindMaximum(T value1, T value2, T value3) 
 	{
-		Integer intNumber1 = 30;
-		Integer intNumber2 = 100;
-		Integer intNumber3 = 80;
+		super();
+		this.value1 = value1;
+		this.value2 = value2;
+		this.value3 = value3;
+	}
 
-		Double doubleNumber1 = 802.3; 
-		Double doubleNumber2 = 803.112; 
-		Double doubleNumber3 = 802.90; 
-
-		String fruit1 = "Apple";
-		String fruit2 = "Banana";
-		String fruit3 = "Peach";
-
-		findMaximum(fruit1, fruit2, fruit3);
-		findMaximum(intNumber1, intNumber2, intNumber3);
-		findMaximum(doubleNumber1, doubleNumber2, doubleNumber3);
+	public void findMax() 
+	{
+		findMaximum(this.value1, this.value2, this.value3);
 	}
 
 	public static <T extends Comparable<T>> void findMaximum(T value1, T value2, T value3) 
@@ -33,5 +31,27 @@ public class FindMaximum
 			maximum = value3;
 		}
 		System.out.println("The Maximum is "+ maximum);
+	}
+
+	public static void main(String[] args)
+	{
+		Integer intNumber1 = 30;
+		Integer intNumber2 = 100;
+		Integer intNumber3 = 800;
+
+		Double doubleNumber1 = 802.3; 
+		Double doubleNumber2 = 803.912; 
+		Double doubleNumber3 = 802.90; 
+
+		String fruit1 = "Apple";
+		String fruit2 = "Banana";
+		String fruit3 = "Peach";
+
+		FindMaximum<Integer> integer = new FindMaximum<Integer>(intNumber1,intNumber2,intNumber3);
+		integer.findMax();
+		FindMaximum<Double> floatingNumber = new FindMaximum<Double>(doubleNumber1,doubleNumber2,doubleNumber3);
+		floatingNumber.findMax();
+		FindMaximum<String> stringValue  = new FindMaximum<String>(fruit1,fruit2,fruit3);
+		stringValue.findMax();
 	}
 }
